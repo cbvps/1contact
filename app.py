215780cbfb3eab272892c5aec9e3800e814157fb
cbvps <119672509+cbvps@users.noreply.github.com>
2025-09-22 14:24:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,8 @@ END:VCARD
         headers={"Content-Disposition": f"attachment; filename={card_id}.vcf"}
     )
 
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
